769fc23ed61c4a1d9776dbe89ce6ad4516c145c6
Veikka Niemi <vegerekka173@gmail.com>
2026-05-17 21:05:46 +0300

    try config

--- cyberhub/src/firebase/firebaseConfig.js
+++ cyberhub/src/firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  VITE_FIREBASE_API_KEY: "AIzaSyAFuCOuh21C1sKSLGExxdpKm-ZP8yRgijY",
+  VITE_FIREBASE_AUTH_DOMAIN: "hartyo-4ef40.firebaseapp.com",
+  VITE_FIREBASE_PROJECT_ID: "hartyo-4ef40",
+  VITE_FIREBASE_STORAGE_BUCKET: "hartyo-4ef40.appspot.com",
+  VITE_FIREBASE_MESSAGING_SENDER_ID: "499688783540",
+  VITE_FIREBASE_APP_ID: "1:499688783540:web:45b7607e9693d62815e1e1"
 };
 console.log("Firebase project:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
 console.log(
